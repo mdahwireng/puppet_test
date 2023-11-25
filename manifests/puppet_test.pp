97e1006d0f482b22fffdcf53d4f3ce2910f89db0
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2023-11-25 12:08:50 +0000

    Rectifies ensure error in manifest

--- manifests/puppet_test.pp
+++ manifests/puppet_test.pp
@@ -1,4 +1,4 @@
-file { '/etc/puppet/code/environment/test_output':
+file { '/etc/puppet/code/environment/test_output/success.txt':
 	ensure => file,
 	content => "The puppet test was successful\n",
 }
